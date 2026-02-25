ad51c76de1e6fd2316e7047f6cc3d3635d1fa2e9
Luciana Roncarati <luroncarati@gmail.com>
2026-02-25 19:36:35 -0300

    User entitites and resource

---
+++ src/main/java/com/aulaspring/projeto1/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.aulaspring.projeto1.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulaspring.projeto1.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserResource {
+	@GetMapping
+	public ResponseEntity <User> findAll() {
+		User u = new User(1L, "Maria","maria@hotmai.com","99999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
+
+
+
+
